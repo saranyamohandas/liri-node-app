5bce18f1bf4fef99c19dc70637d7af1e9df4ee50
saranyamohandas <saranyamohandas2@gmail.com>
2018-12-10 20:50:29 -0800

    update liri

--- liri.js
+++ liri.js
@@ -56,21 +56,10 @@ function callSpotifyAPI(searchqry){
     if(!searchqry && !randomTask){
         trackQry = "The Sign";
     } else if(randomTask){
-        trackQry = 
-        
+        trackQry = randomQry;    
     } else {
-          searchqry.join(" ")
-//        var getQry = buildQry();
-//        for(i=3;i<searchqry.length;i++){
-//        if(i != searchqry.length-1){   
-//             trackQry += searchqry[i] + " ";
-//             //console.log(movieQry);
-//         } else {
-//             trackQry += searchqry[i]
-//             //console.log("else", movieQry);
-//         };
-//    };
-        
+        trackQry = searchqry.join(" ")
+    
     }
     
     console.log("trackQry-" ,trackQry);
@@ -78,26 +67,25 @@ function callSpotifyAPI(searchqry){
     spotify.search({
     type: "track",
     query : trackQry
-    //limit : 1
     
-}).then(function(res){
-    //console.log(res.tracks.items[0].artists[0].name);
-//    console.log(res.tracks.items[0].artists);
-   
+    }).then(function(res){
+    
     var getItems = res.tracks.items;
 
     for(i=0;i<getItems.length;i++){
         
         if(getItems[i].name.toLowerCase() == trackQry.toLowerCase()){
-            console.log("i-" + i, getItems[i].artists[0].name);
+            //console.log("i-" + i, getItems[i].artists[0].name);
              //Artist name
+            console.log("**************************************");
             console.log("Artist:",getItems[i].artists[0].name);
             //Song preview 
-            console.log("Preview URL",getItems[i].preview_url)
+            console.log("Preview URL:",getItems[i].preview_url)
             //Song name 
             console.log("Song Name:",getItems[i].name)
             //Album name 
             console.log("Album:",getItems[i].album.name)
+            console.log("**************************************");
             //trackMatch = true;
             //console.log(i);
             break;
@@ -113,26 +101,24 @@ function callSpotifyAPI(searchqry){
 function callBandsInTownAPI(searchqry){
     
     if(!searchqry){
-        artistQry = ""; //check requirement
+        //artistQry = ""; //check requirement
+        console.log("Please enter an Artist for search!!")
     } else {
-        searchqry.join(" ")
-        //var artistQry = buildQry(searchqry);
-    }
-    
-// API call using request   
-request("https://rest.bandsintown.com/artists/" + artistQry + "/events?app_id=codingbootcamp", function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //build string
+        artistQry = searchqry.join(" ");
+        // API call using request   
+        request("https://rest.bandsintown.com/artists/" + artistQry + "/events?app_id=codingbootcamp", function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
  
-    var concertRes = JSON.parse(body);
-    console.log(concertRes.length)
-    
-    console.log("Name of the venue : ",concertRes[0].venue.name);
-    console.log("Venue location : ",concertRes[0].venue.city + "," +  concertRes[0].venue.region +","+ concertRes[0].venue.country );
-    console.log("Date of the Event : " , moment(concertRes[0].datetime).format("L"));
+            var concertRes = JSON.parse(body);
+            console.log(concertRes.length)
 
-});
-    
+            console.log("Name of the venue : ",concertRes[0].venue.name);
+            console.log("Venue location : ",concertRes[0].venue.city + "," +  concertRes[0].venue.region +","+ concertRes[0].venue.country );
+            console.log("Date of the Event : " , moment(concertRes[0].datetime).format("L"));
+        });
+    }
 }
 
 
@@ -199,7 +185,8 @@ function callRandom(){
             randomTask = true;
             randomQry = getData[1];
             callAPI(getData[0]);
-    }
+    })
+}
 
 
 // build search string for track/bands search
